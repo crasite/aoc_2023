cabf450820c150c44ad4e913d8e98c229c3cb5c2
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-12-04 13:02:24 +0700

    day4: part2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,8 +1,11 @@
+use std::collections::{HashSet, HashMap};
+
 mod parser;
 
 fn main(){
     let input = include_str!("input.txt");
     println!("Part 1: {}", solve(input));
+    println!("Part 2: {}", solve2(input));
 }
 
 fn solve(input: &'static str) -> u32 {
@@ -14,6 +17,34 @@ fn solve(input: &'static str) -> u32 {
     cards.iter().map(|card| card.get_score()).sum()
 }
 
+fn solve2(input: &'static str) -> u32 {
+    let mut cards = Vec::new();
+    let mut card_map = HashMap::new();
+    for line in input.lines() {
+        let card = parser::parse_card(line).unwrap().1;
+        cards.push(card);
+    }
+    let max = cards.len();
+    for card in &cards {
+        match card_map.get(&card.id) {
+            Some(v) => card_map.insert(card.id, v + 1),
+            None => card_map.insert(card.id, 1),
+        };
+        let next_cards = card.get_next_cards();
+        let total = card_map.get(&card.id).unwrap().clone();
+        for next_card in next_cards {
+            if next_card > max as u32 {
+                break;
+            }
+            match card_map.get(&next_card) {
+                Some(v) => card_map.insert(next_card, v + total),
+                None => card_map.insert(next_card, total),
+            };
+        };
+    }
+    card_map.iter().fold(0, |acc, (_, v)| acc + v)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +53,11 @@ mod tests {
     fn could_solve_part1() {
         let input = include_str!("sample.txt");
         assert_eq!(solve(input), 13);
+    }
+
+    #[test]
+    fn could_solve_part2() {
+        let input = include_str!("sample.txt");
+        assert_eq!(solve2(input), 30);
     }
 }
--- src/bin/day4/parser.rs
+++ src/bin/day4/parser.rs
@@ -1,24 +1,31 @@
 use nom::IResult;
 
-
 #[derive(Debug, PartialEq)]
 pub struct Card {
-    id: u32,
+    pub id: u32,
     winning_numbers: Vec<u32>,
     playing_numbers: Vec<u32>,
 }
 
 impl Card {
-   pub  fn get_score(&self) -> u32 {
-        let correct = self.winning_numbers
+    fn correct_number_count(&self) -> u32 {
+        self.winning_numbers
             .iter()
             .filter(|num| self.playing_numbers.contains(num))
-            .count() as u32;
+            .count() as u32
+    }
+
+    pub fn get_score(&self) -> u32 {
+        let correct = self.correct_number_count();
         if correct == 0 {
             0
         } else {
             2u32.pow(correct - 1)
         }
+    }
+
+    pub fn get_next_cards(&self) -> Vec<u32> {
+        (self.id + 1..=self.id + self.correct_number_count()).collect()
     }
 }
 
@@ -28,13 +35,25 @@ pub fn parse_card(input: &'static str) -> IResult<&str, Card> {
     let (input, id) = nom::character::complete::u32(input)?;
     let (input, _) = nom::bytes::complete::tag(":")(input)?;
     let (input, _) = nom::character::complete::space0(input)?;
-    let (input, winning_numbers) = nom::multi::separated_list1(nom::character::complete::space1, nom::character::complete::u32)(input)?;
+    let (input, winning_numbers) = nom::multi::separated_list1(
+        nom::character::complete::space1,
+        nom::character::complete::u32,
+    )(input)?;
     let (input, _) = nom::bytes::complete::tag(" |")(input)?;
     let (input, _) = nom::character::complete::space0(input)?;
-    let (input, playing_numbers) = nom::multi::separated_list1(nom::character::complete::space1, nom::character::complete::u32)(input)?;
-    Ok((input, Card { id, winning_numbers, playing_numbers }))
+    let (input, playing_numbers) = nom::multi::separated_list1(
+        nom::character::complete::space1,
+        nom::character::complete::u32,
+    )(input)?;
+    Ok((
+        input,
+        Card {
+            id,
+            winning_numbers,
+            playing_numbers,
+        },
+    ))
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -59,5 +78,16 @@ mod tests {
             playing_numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
         };
         assert_eq!(card.get_score(), 8);
+    }
+
+    #[test]
+    fn could_get_next_cards() {
+        let card = Card {
+            id: 3,
+            winning_numbers: vec![41, 48, 83, 86, 17],
+            playing_numbers: vec![83, 86, 6, 31, 17, 9, 48, 53],
+        };
+        let expected = vec![4, 5, 6, 7];
+        assert_eq!(card.get_next_cards(), expected);
     }
 }
