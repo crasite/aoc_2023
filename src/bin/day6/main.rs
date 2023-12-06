2472ddfe17326065aeed5ecc2eebf47819f148b6
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-12-06 15:07:42 +0700

    day6: part2

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -1,18 +1,25 @@
 mod parser;
 
-fn main(){
+fn main() {
     let input = include_str!("./input.txt");
     println!("Part 1: {}", solve_part_1(input));
+    println!("Part 2: {}", solve_part_2(input));
 }
 
-fn solve_part_1(input: &str) -> i32 {
+fn solve_part_1(input: &str) -> u64 {
     let rs = parser::parse_game(input).unwrap().1;
     let mut total = 1;
     for r in rs {
         let (least, max) = r.winning_hold_duration();
-        total *= max-least+1
+        total *= max - least + 1
     }
     total
+}
+
+fn solve_part_2(input: &str) -> u64 {
+    let race = parser::parse_game2(input).unwrap().1;
+    let (min, max) = race.winning_hold_duration();
+    max - min + 1
 }
 
 #[cfg(test)]
@@ -23,5 +30,11 @@ mod tests {
     fn could_solve_part_1() {
         let input = include_str!("./sample.txt");
         assert_eq!(solve_part_1(input), 288);
+    }
+
+    #[test]
+    fn could_solve_part_2() {
+        let input = include_str!("./sample.txt");
+        assert_eq!(solve_part_2(input), 71503);
     }
 }
--- src/bin/day6/parser.rs
+++ src/bin/day6/parser.rs
@@ -1,17 +1,22 @@
 use aoc::space0_surrounded;
-use nom::{IResult, bytes::complete::tag, multi::{separated_list1, self, many1}, character::complete};
+use nom::{
+    bytes::complete::{tag, take_till1},
+    character::complete,
+    multi::{many1, separated_list1},
+    IResult,
+};
 
 #[derive(Debug, PartialEq)]
 pub struct Race {
-    time: i32,
-    distance: i32,
+    time: u64,
+    distance: u64,
 }
 impl Race {
-    pub fn winning_hold_duration(&self) -> (i32, i32) {
+    pub fn winning_hold_duration(&self) -> (u64, u64) {
         let mut first_value = None;
         let mut last_value = None;
         for i in 1..self.time {
-            let distance = i*(self.time-i);
+            let distance = i * (self.time - i);
             if distance > self.distance {
                 if first_value.is_none() {
                     first_value = Some(i);
@@ -24,19 +29,38 @@ impl Race {
     }
 }
 
-pub fn parse_game(input: &str) -> IResult<&str, Vec<Race>>{
+pub fn parse_game(input: &str) -> IResult<&str, Vec<Race>> {
     let (input, _) = tag("Time:")(input)?;
-    let (input, time_list) = many1(space0_surrounded(complete::i32))(input)?;
+    let (input, time_list) = many1(space0_surrounded(complete::u64))(input)?;
     let (input, _) = tag("\nDistance:")(input)?;
-    let (input, distance_list) = many1(space0_surrounded(complete::i32))(input)?;
+    let (input, distance_list) = many1(space0_surrounded(complete::u64))(input)?;
     let mut rs = vec![];
     for i in 0..time_list.len() {
-        rs.push(Race{
+        rs.push(Race {
             time: time_list[i],
             distance: distance_list[i],
         })
     }
     Ok((input, rs))
+}
+
+fn parse_number(input: &str) -> IResult<&str, u64> {
+    let (input, _) = take_till1(|c: char| c.is_ascii_digit())(input)?;
+    let (input, number) = take_till1(|c: char| c == '\n')(input)?;
+    let number = number.replace(' ', "");
+    let number = number.parse::<u64>().unwrap();
+    Ok((input, number))
+}
+
+pub fn parse_game2(input: &str) -> IResult<&str, Race> {
+    let (input, value) = separated_list1(complete::newline, parse_number)(input)?;
+    Ok((
+        input,
+        Race {
+            time: value[0],
+            distance: value[1],
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -64,12 +88,22 @@ mod tests {
     }
 
     #[test]
+    fn could_parse_race2() {
+        let input = include_str!("./sample.txt");
+        let output = Race {
+            time: 71530,
+            distance: 940200,
+        };
+        assert_eq!(parse_game2(input).unwrap().1, output);
+    }
+
+    #[test]
     fn could_calculate_winning_duration() {
         let race = Race {
             time: 7,
             distance: 9,
         };
-        let output = (2,5);
+        let output = (2, 5);
         assert_eq!(race.winning_hold_duration(), output);
     }
 }
